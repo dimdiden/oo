cd91963d27a8875a131fc6279c31787af84fb72d
Your Name <dimdiden@gmail.com>
2018-08-22 17:53:58 +0300

    Changed naming, added comments

--- client.go
+++ client.go
@@ -13,43 +13,43 @@ import (
 	"time"
 )
 
-// Clent holds secret key, api key, basic url path and expire window in hours
-// Client is needed to make basic requests to Ooayla APIs
-type Client struct {
-	// Secret is the secret key for Ooyala account
-	Secret string
-	// Api is the api key for Ooyala account
-	Api string
-	// Url is the root url for making requests:
+// Api holds secret key, api key, basic url path and expire window in hours
+// Api is needed to make basic requests to Ooayla APIs
+type Api struct {
+	// Skey is the secret key for Ooyala account
+	Skey string
+	// Akey is the api key for Ooyala account
+	Akey string
+	// RootUrl is the root url for making requests:
 	// For example https://api.ooyala.com for Backlot REST api
 	RootUrl *url.URL
 	// Delta is the number of hours the request should stay valid
+	// Required further to generate expires value for a request
 	Delta int
 }
 
-// NewClient returns the pointer to the new instance of the client
-func NewClient(secret, api, root string, delta int) (*Client, error) {
-	client := &Client{Secret: secret, Api: api, Delta: delta}
+// NewApi returns the pointer to the new instance of the Api object
+func NewApi(skey, akey, root string, delta int) (*Api, error) {
+	api := &Api{Skey: skey, Akey: akey, Delta: delta}
 	u, err := url.Parse(root)
 	if err != nil {
 		return nil, err
 	}
-	client.RootUrl = u
-	return client, nil
+	api.RootUrl = u
+	return api, nil
 }
 
-// type Request struct {
-// 	Url *url.URL
-// }
-
-func (c Client) NewRequest(method, path string, body io.Reader) (*http.Request, error) {
-
+// NewRequest takes http method in lower or upper case, url query with parameters, and
+// body as any Reader and returns *http.Request ready for sending by the http client
+func (c Api) NewRequest(method, path string, body io.Reader) (*http.Request, error) {
+	// Convert body to bytes.Buffer for passing it as string to Sign
 	var buf bytes.Buffer
 	buf.ReadFrom(body)
-
+	// Get the signed url.URL
 	u, err := c.Sign(method, path, buf.String())
+	// Make full url with subpath and query
 	u = c.RootUrl.ResolveReference(u)
-
+	// Make a simple request
 	req, err := http.NewRequest(method, u.String(), &buf)
 	if err != nil {
 		return nil, err
@@ -57,17 +57,20 @@ func (c Client) NewRequest(method, path string, body io.Reader) (*http.Request, 
 	return req, nil
 }
 
-func (c Client) Sign(method, path string, body string) (*url.URL, error) {
+// Sign gets a raw url query, adds api_key and expires values,
+// generate and adds signatture for the query an returns url.URL adding the new values
+func (c Api) Sign(method, path string, body string) (*url.URL, error) {
+	// Convert string path to url.URL value
 	u, err := url.Parse(path)
 	if err != nil {
 		return nil, err
 	}
 	// Perform initial string concantination
-	sig := c.Secret + strings.ToUpper(method) + u.Path
-	// Get the query parameters
+	sig := c.Skey + strings.ToUpper(method) + u.Path
+	// Get the query parameters and add api and expires there
 	q := u.Query()
-	q.Set("api_key", c.Api)
-	q.Set("expires", c.Expires())
+	q.Set("api_key", c.Akey)
+	q.Set("expires", c.expires())
 	// Sort url parameters by keys alphabetically and contaninate them like a=1b=2c=3
 	var keys []string
 	for k := range q {
@@ -81,14 +84,16 @@ func (c Client) Sign(method, path string, body string) (*url.URL, error) {
 	sig += body
 	sum := sha256.Sum256([]byte(sig))
 	sig = string(base64.StdEncoding.EncodeToString(sum[:]))[:43]
+	// Adding signature to the query parameters and encode them
 	q.Set("signature", sig)
+	// Reassing query with all parameters to the url again
 	u.RawQuery = q.Encode()
-
 	return u, nil
 }
 
-func (c Client) Expires() string {
-	// Get the expires value by adding c.Delta to the current time
+// Expires genrates expires value based on Delta value
+func (c Api) expires() string {
+	// Generate the expires value by adding c.Delta to the current time
 	timestamp := time.Now().Add(time.Hour * time.Duration(c.Delta)).Unix()
 	expires := strconv.FormatInt(timestamp, 10)
 	return expires
